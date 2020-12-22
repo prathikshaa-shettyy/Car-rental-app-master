7c44ae6ae144de90ee119b95b14310b53be11779
Prathiksha shetty <prathikshashetty47@gmail.com>
2020-12-22 20:26:59 +0530

    Checkpoint 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { SignInComponent } from './shared/sign-in/sign-in.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './core/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    SignInComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
